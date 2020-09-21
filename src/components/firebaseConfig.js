b08cd254f59ba8121a93509a270ffe7b2386ef94
Shubham Kashikar <54555834+skashika@users.noreply.github.com>
2020-09-21 15:50:25 -0400

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default{
+  apiKey: "AIzaSyA_omrzfV6RsZSE2jvujcnc9tALSrByiZ8",
+  authDomain: "employee-info-336d3.firebaseapp.com",
+  databaseURL: "https://employee-info-336d3.firebaseio.com",
+  projectId: "employee-info-336d3",
+  storageBucket: "employee-info-336d3.appspot.com",
+  messagingSenderId: "787001850906",
+  appId: "1:787001850906:web:25690392f86354edc3a5a3",
+  measurementId: "G-6PKPHDGQKQ"
+}
---
+++ src/components/newEmployee.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="new-employee">
+    <h3>New Employee</h3>
+    <div class = "row">
+      <form @submit.prevent="saveEmployee" class="col s12">
+        <!-- ID -->
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="employee_id" required>
+            <label>Employee ID number</label>
+          </div>
+        </div>
+        <!-- Name -->
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="name" required>
+            <label>Name</label>
+          </div>
+        </div>
+        <!-- Department -->
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="dept" required>
+            <label>dept</label>
+          </div>
+        </div>
+        <!-- Entry for Position-->
+        <div class="row">
+          <div class="input-field col s12">
+            <input type="text" v-model="position" required>
+            <label>Position</label>
+          </div>
+        </div>
+        <button type="submit" class="btn">Submit</button>
+        <router-link to="/" class="btn grey">Cancel</router-link>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import db from './firebaseinit'
+export default {
+  name: 'new-employee',
+  data(){
+    return{
+      employee_id: null,
+      name: null,
+      dept: null,
+      position: null
+    }
+  },
+  methods:{
+    saveEmployee(){
+      db.collection('employees').add({
+        employee_id: this.employee_id,
+        name: this.name,
+        dept: this.dept,
+        position: this.position
+        })
+        .then(docRef => {
+          this.$router.push('/')
+        }).catch(error => console.log('Something wrong happened'))
+    }
+  }
+}
+</script>
